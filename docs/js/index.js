1f052fa12a66e1b717fd560c56d8211a2461a4bc
NevidJimenez <49883970+NevidJimenez@users.noreply.github.com>
2020-11-07 02:34:58 -0600

    we add scroll reveal

---
+++ docs/js/index.js
@@ -0,0 +1,43 @@
+window.sr = ScrollReveal();
+
+  sr.reveal('.navbar', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-50px'
+  });
+
+    sr.reveal('.carrusel', {
+    duration: 2000,
+    origin: 'left',
+    distance: '-200px'
+  });
+
+    sr.reveal('.quienessomos', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-50px'
+  });
+
+    sr.reveal('.historia', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-50px'
+  });
+
+    sr.reveal('.vision', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-50px'
+  });
+
+    sr.reveal('.mision', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-50px'
+  });
+
+    sr.reveal('.contacto', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-50px'
+  });
